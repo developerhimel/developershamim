440fccf59c7cc6a2261ef8d10e136dcf4fff3656
developerhimel <freelancerhimel2814@gmail.com>
2021-08-23 10:40:56 +0600

    testV1.1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -13,11 +13,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Developer Shamim!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -54,16 +54,16 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://www.teamdh.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          <span style={{marginTop:0}}>Powered by </span>
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src="/dh.png" alt="DH Logo" width={30} height={30} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
